db07157499d7dbb20fc968ea04df3d929b9b0c86
anukebi <chumburidze.anukebi@kiu.edu.ge>
2026-06-20 17:52:05 +0400

    Fix argument parsing for game start

--- src/main/java/edu/kiu/uno/game/GameConfig.java
+++ src/main/java/edu/kiu/uno/game/GameConfig.java
@@ -12,21 +12,21 @@ import lombok.ToString;
 public class GameConfig {
 
   private static final Function<GameConfig, List<BiFunction<String[], Integer, Integer>>> CONFIG_SETTERS = g -> List.of(
-      g::setBots, g::setGames, g::setSeed, g::setHuman, g::setQuiet, g::setSelfTest, g::setHelp
+      g::setBots, g::setGames, g::setSeed, g::setHuman, g::setQuiet, g::setHelp
   );
 
-  private int bots;
-  private int games;
-  private long seed;
-  private boolean human;
-  private boolean quiet;
-  private boolean selfTest;
-  private boolean help;
+  private int bots = 3;
+  private int games = 1;
+  private long seed = System.currentTimeMillis();
+  private boolean human = false;
+  private boolean quiet = false;
+  private boolean help = false;
 
   public GameConfig(String[] args) {
     var setters = CONFIG_SETTERS.apply(this);
     for (int i = 0; i < args.length; i++) {
       for (var setter : setters) {
+        System.err.println("GameConfig:: Processing argument: " + args[i] + " with setter: " + setter + " at index: " + i);
         var newIndex = setter.apply(args, i);
         if (newIndex != i) {
           break;
@@ -38,41 +38,36 @@ public class GameConfig {
   private int setBots(String[] args, int i) {
     bots = args[i].equals("--bots") && hasNextArg(args, i)
         ? Integer.parseInt(args[++i])
-        : 3;
+        : bots;
     return i;
   }
 
   private int setGames(String[] args, int i) {
     games = args[i].equals("--games") && hasNextArg(args, i)
         ? Integer.parseInt(args[++i])
-        : 1;
+        : games;
     return i;
   }
 
   private int setSeed(String[] args, int i) {
     seed = args[i].equals("--seed") && hasNextArg(args, i)
         ? Long.parseLong(args[++i])
-        : System.currentTimeMillis();
+        : seed;
     return i;
   }
 
   private int setHuman(String[] args, int i) {
-    human = args[i].equals("--human");
+    human |= args[i].equals("--human");
     return i;
   }
 
   private int setQuiet(String[] args, int i) {
-    quiet = args[i].equals("--quiet");
-    return i;
-  }
-
-  private int setSelfTest(String[] args, int i) {
-    selfTest = args[i].equals("--self-test");
+    quiet |= args[i].equals("--quiet");
     return i;
   }
 
   private int setHelp(String[] args, int i) {
-    help = args[i].equals("--help");
+    help |= args[i].equals("--help");
     return i;
   }
 
